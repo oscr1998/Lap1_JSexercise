eed3c722c632fa388448173956ccfb98d0322d03
Oliver Catelan-Robinson <oscr1998@outlook.com>
2022-09-20 11:39:55 +0100

    first commit

---
+++ exercise.js
@@ -0,0 +1,35 @@
+// 1.
+
+// function stringLength(word){
+//     console.log(word.length)
+// }
+
+// stringLength('hello')
+
+// #########################
+
+function getTrain(sentence){
+    let splitSen = sentence.split(' ')
+    for(i=0; i<splitSen.length; i++){
+        if(splitSen[i].toLowerCase() === 'trains'){
+            return splitSen[i]
+        }
+    }
+}
+
+console.log(getTrain('I dislike trains as a mode of transport'))
+
+// ########################
+
+function fifth(letters){
+    return letters[4]
+}
+
+console.log(fifth('abcdefghijklmnopqrstuvwxyz'))
+
+// ##################
+function upper(word){
+    return word.toUpperCase()
+}
+
+console.log(upper('I love you'))
