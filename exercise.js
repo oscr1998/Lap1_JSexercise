0b86d372a25375cc72877d2bb2a03341e18b1184
ahmed <ahmedmuhammed78613@gmail.com>
2022-09-20 11:48:52 +0100

    update 2

--- exercise.js
+++ exercise.js
@@ -8,31 +8,32 @@
 
 // #########################
 
-function getTrain(sentence){
-    let splitSen = sentence.split(' ')
-    for(i=0; i<splitSen.length; i++){
-        if(splitSen[i].toLowerCase() === 'trains'){
-            return splitSen[i]
-        }
-    }
+function getTrain(sentence) {
+	let splitSen = sentence.split(" ");
+	for (i = 0; i < splitSen.length; i++) {
+		if (splitSen[i].toLowerCase() === "trains") {
+			return splitSen[i];
+		}
+	}
 }
 
-console.log(getTrain('I dislike trains as a mode of transport'))
+console.log(getTrain("I dislike trains as a mode of transport"));
 
 // ########################
 
-function fifth(letters){
-    return letters[4]
+function fifth(letters) {
+	return letters[4];
 }
 
-console.log(fifth('abcdefghijklmnopqrstuvwxyz'))
+console.log(fifth("abcdefghijklmnopqrstuvwxyz"));
 
 // ##################
-function upper(word){
-    return word.toUpperCase()
+function upper(word) {
+	return word.toUpperCase();
 }
 
-console.log(upper('I love you'))
+console.log(upper("I love you"));
 
+// TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
 
-// TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST 
+/* another <change className=""></change> */
